9b7875ad4f4269f9724b9d93d81f8575fc29e253
Renan Santana <renan_ota@hotmail.com>
2024-08-21 11:41:28 -0300

    Format code

--- src/database.py
+++ src/database.py
@@ -4,7 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = config("DATABASE_URL",)
+DATABASE_URL = config(
+    "DATABASE_URL",
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
